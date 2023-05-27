5e11cffb121b361ed58262833017083496b60d79
srua <srua029@misena.edu.co>
2023-05-27 18:22:05 -0500

    si

--- src/app/views/paquete/list-paquetes/list-paquetes.component.ts
+++ src/app/views/paquete/list-paquetes/list-paquetes.component.ts
@@ -8,7 +8,8 @@ import { UsuarioInterface } from 'src/app/models/usuario.interface';
 import { ClienteInterface } from 'src/app/models/cliente.interface';
 import { EstadoPaqueteInterface } from 'src/app/models/estado-paquete.interface';
 import { MatDialog } from '@angular/material/dialog';
-import { DialogConfirmComponent, ConfirmDialogData } from '../../../templates/navigation/dialog-confirm/dialog-confirm.component';
+import { DialogConfirmComponent, ConfirmDialogData } from '../../../templates/dialog-confirm/dialog-confirm.component';
+import { LoadingComponent } from 'src/app/templates/loading/loading.component';
 
 @Component({
   selector: 'app-list-paquetes',
